3836d70a722e3a2d1ebae0c30c5fea1a6cebcaea
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-08-19 21:09:42 +0800

    Revert "UPSTREAM: vfs: split out access_override_creds()"
    This reverts commit 353f0e7ae126975330b9cac88478bb8bb6eb29d9.

--- fs/open.c
+++ fs/open.c
@@ -305,14 +305,21 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
  * We do this by temporarily clearing all FS-related capabilities and
  * switching the fsuid/fsgid around to the real ones.
  */
-static const struct cred *access_override_creds(void)
+long do_faccessat(int dfd, const char __user *filename, int mode)
 {
 	const struct cred *old_cred;
 	struct cred *override_cred;
+	struct path path;
+	struct inode *inode;
+	int res;
+	unsigned int lookup_flags = LOOKUP_FOLLOW;
+
+	if (mode & ~S_IRWXO)	/* where's F_OK, X_OK, W_OK, R_OK? */
+		return -EINVAL;
 
 	override_cred = prepare_creds();
 	if (!override_cred)
-		return NULL;
+		return -ENOMEM;
 
 	override_cred->fsuid = override_cred->uid;
 	override_cred->fsgid = override_cred->gid;
@@ -347,28 +354,6 @@ static const struct cred *access_override_creds(void)
 	override_cred->non_rcu = 1;
 
 	old_cred = override_creds(override_cred);
-
-	/* override_cred() gets its own ref */
-	put_cred(override_cred);
-
-	return old_cred;
-}
-
-long do_faccessat(int dfd, const char __user *filename, int mode)
-{
-	struct path path;
-	struct inode *inode;
-	int res;
-	unsigned int lookup_flags = LOOKUP_FOLLOW;
-	const struct cred *old_cred;
-
-	if (mode & ~S_IRWXO)	/* where's F_OK, X_OK, W_OK, R_OK? */
-		return -EINVAL;
-
-	old_cred = access_override_creds();
-	if (!old_cred)
-		return -ENOMEM;
-
 retry:
 	res = user_path_at(dfd, filename, lookup_flags, &path);
 	if (res)
@@ -412,6 +397,7 @@ out_path_release:
 	}
 out:
 	revert_creds(old_cred);
+	put_cred(override_cred);
 	return res;
 }
 
